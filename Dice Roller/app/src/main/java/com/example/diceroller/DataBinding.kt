df4ee8f44f1ffd1dd960573806c798b70ac67b21
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2023-02-12 05:07:55 +0530

    Added Data Binding - Day 3

---
+++ Dice Roller/app/src/main/java/com/example/diceroller/DataBinding.kt
@@ -0,0 +1,3 @@
+package com.example.diceroller
+
+data class DataBinding(val diceText : String)
--- Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Dice Roller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,28 +1,33 @@
 package com.example.diceroller
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.diceroller.databinding.ActivityMainBinding
+
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        // created variable rollnobutton to access the xml data, we accessed from R class which stores all values in int
-        var rollNoButton: Button = findViewById(R.id.roll_btn)
-        var Dice_Number_txtview: TextView = findViewById(R.id.Dice_Number_txtview)
-        val diceImageView: ImageView = findViewById(R.id.diceImageView)
-        //rollNoButton.text = "Text Changed" //Static Programmatically XML Changed
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val dataBindingObj = DataBinding("Roll your Dice") //
+        binding.data = dataBindingObj //
+
+
         var total = 0
 
         fun random_no(): Unit {
             val randomValues = Random.nextInt(0, 6) + 1 // as dice value start from 1 - 6
             val roll = randomValues
             when (roll) {
+
+                //  Debug
 //                1 -> Log.d("Test Debug", "Print 1")
 //                2 -> Log.d("Test Debug", "Print 2")
 //                3 -> Log.d("Test Debug", "Print 3")
@@ -30,25 +35,29 @@ class MainActivity : AppCompatActivity() {
 //                5 -> Log.d("Test Debug", "Print 5")
 //                6 -> Log.d("Test Debug", "Print 6")
 
-                1 -> diceImageView.setImageResource(R.drawable.dice_roll_1)
-                2 -> diceImageView.setImageResource(R.drawable.dice_roll_2)
-                3 -> diceImageView.setImageResource(R.drawable.dice_roll_3)
-                4 -> diceImageView.setImageResource(R.drawable.dice_roll_4)
-                5 -> diceImageView.setImageResource(R.drawable.dice_roll_5)
-                6 -> diceImageView.setImageResource(R.drawable.dice_roll_6)
+                1 -> binding.diceImageView.setImageResource(R.drawable.dice_roll_1)
+                2 -> binding.diceImageView.setImageResource(R.drawable.dice_roll_2)
+                3 -> binding.diceImageView.setImageResource(R.drawable.dice_roll_3)
+                4 -> binding.diceImageView.setImageResource(R.drawable.dice_roll_4)
+                5 -> binding.diceImageView.setImageResource(R.drawable.dice_roll_5)
+                6 -> binding.diceImageView.setImageResource(R.drawable.dice_roll_6)
 
 
             }
             total = total + randomValues
-            Dice_Number_txtview.text =
-                "Dice Rolled = " + randomValues.toString() + "\nTotal = " + total
+//            Dice_Number_txtview.text =
+//                "Dice Rolled = " + randomValues.toString() + "\nTotal = " + total
 
-            Toast.makeText(this, total.toString(), Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                this,
+                "Dice Rolled = " + roll + "\n Total since roll =" + total.toString(),
+                Toast.LENGTH_SHORT
+            ).show()
 
         }
 
 
-        rollNoButton.setOnClickListener() {
+        binding.rollBtn.setOnClickListener() {
             random_no()
         }
     }
