0af31d7e10bf3b7a0a32c5bc317a46a03864af61
Neel Doshi <60827173+neeldoshii@users.noreply.github.com>
2023-03-07 01:56:06 +0530

    Room DataBase

--- Room/app/src/main/java/com/example/room/MainActivity.kt
+++ Room/app/src/main/java/com/example/room/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.room.Room
 import kotlinx.coroutines.*
 
@@ -17,19 +19,20 @@ class MainActivity : AppCompatActivity() {
 
         GlobalScope.launch {
             database.contactDao().insertContact(Contact(1,"Neel","Doshi",99999))
+            database.contactDao().insertContact(Contact(2,"Neel","Doshi",99999))
 
         }
 
 
 
     }
+
+    fun getContacts(view: View) {
+
+        database.contactDao().getAllContact().observe(this,{
+            Log.d("Tag-Neel", it.toString())
+        })
+    }
 }
 
 
-//GetBudgetTuned .com
-//thefinancebanking.com
-//fortunedollar.com
-
-//fortunedollarblog.com
-
-//getfinancewise.com
